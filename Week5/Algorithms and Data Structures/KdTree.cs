03a699d74c7e115207bf065df99191c8e5b36f67
Lucas Toledo <toledo.lucas06@gmail.com>
2023-11-10 18:02:53 -0300

    Adding Week5 algorithms

---
+++ Week5/Algorithms and Data Structures/KdTree.cs
@@ -0,0 +1,166 @@
+﻿namespace KdTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Initialize root node as null
+            Node root = null;
+
+            // Define an array of points to be inserted into the tree
+            int[][] points = {
+            new int[] { 3, 6 },
+            new int[] { 17, 15 },
+            new int[] { 13, 15 },
+            new int[] { 6, 12 },
+            new int[] { 9, 1 },
+            new int[] { 2, 7 },
+            new int[] { 10, 19 }
+            };
+
+            // Get the length of the points array
+            int n = points.Length;
+
+            // Create a new KdTree object with k=2
+            KdTree tree = new KdTree(2);
+
+            // Insert all the points into the tree and update the root node
+            for (int i = 0; i < n; i++)
+            {
+                root = tree.insert(root, points[i]);
+            }
+
+            // Define a point to search for in the tree
+            int[] point1 = { 10, 19 };
+
+            // Search for the point in the tree and print the result
+            if (tree.Search(root, point1))
+            {
+                Console.WriteLine("Found");
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+
+            // Define another point to search for in the tree
+            int[] point2 = { 12, 19 };
+
+            // Search for the point in the tree and print the result
+            if (tree.Search(root, point2))
+            {
+                Console.WriteLine("Found");
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
+    }
+    // Defining the Node class with an integer array to hold the point and references to left and right nodes
+    public class Node
+    {
+        public int[] point;
+        public Node left;
+        public Node right;
+
+        // Defining a constructor to initialize the Node object
+        public Node(int[] point)
+        {
+            this.point = point;
+            left = null;
+            right = null;
+        }
+
+    }
+
+    public class KdTree
+    {
+        private int k;
+
+        // Defining a constructor to initialize the KdTree object
+        public KdTree(int k)
+        {
+            this.k = k;
+        }
+
+        // Defining a method to create a new Node object
+        public static Node NewNode(int[] point)
+        {
+            return new Node(point);
+        }
+
+        // Defining a recursive method to insert a point into the K-d tree
+        public Node InsertRec(Node root, int[] point, int depth)
+        {
+            if (root == null)
+            {
+                return NewNode(point);
+            }
+
+            int cd = depth % k;
+
+            if (point[cd] < root.point[cd])
+            {
+                root.left = InsertRec(root.left, point, depth + 1);
+            }
+            else
+            {
+                root.right = InsertRec(root.right, point, depth + 1);
+            }
+
+            return root;
+        }
+
+        // Defining a method to insert a point into the K-d tree
+        public Node insert(Node root, int[] point)
+        {
+            return InsertRec(root, point, 0);
+        }
+
+        // Defining a method to check if two points are the same
+        public bool arePointsSame(int[] point1, int[] point2)
+        {
+            for (int i = 0; i < k; i++)
+            {
+                if (point1[i] != point2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Defining a recursive method to search for a point in the K-d tree
+        public bool SearchRec(Node root, int[] point, int depth)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (arePointsSame(root.point, point))
+            {
+                return true;
+            }
+
+            int cd = depth % k;
+
+            if (point[cd] < root.point[cd])
+            {
+                return SearchRec(root.left, point, depth + 1);
+            }
+
+            return SearchRec(root.right, point, depth + 1);
+        }
+
+        // Defining a method to search for a point in the K-d tree
+        public bool Search(Node root, int[] point)
+        {
+            return SearchRec(root, point, 0);
+        }
+
+    }
+}
---
+++ Week5/Algorithms and Data Structures/RedBlackBST.cs
@@ -0,0 +1,183 @@
+﻿namespace RedBlackBST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RedBlackBST<string, int> bst = new RedBlackBST<string, int>();
+            bst.Put("S", 1);
+            bst.Put("E", 3);
+            bst.Put("R", 2);
+            bst.Put("X", 4);
+            bst.Put("A", 7);
+            bst.Put("C", 2);
+            bst.Put("H", 4);
+            bst.Put("M", 7);
+
+            foreach (string s in bst.Keys())
+            {
+                Console.Write(s + " ");
+            }
+            Console.Write("\n");
+        }
+    }
+    public class RedBlackBST<Key, Value> where Key : IComparable<Key>
+    {
+        private Node Root;
+        private bool RED = true;
+        private bool BLACK = false;
+
+        private class Node
+        {
+            public Key Key;
+            public Value Value;
+            public Node Left, Right;
+            public bool Color; // Color of parent link
+
+            public Node(Key key, Value value, bool color)
+            {
+                Key = key;
+                Value = value;
+                Color = color;
+            }
+        }
+
+        public Value Get(Key key)
+        {
+            Node node = Root;
+            while (node != null)
+            {
+                int comp = key.CompareTo(node.Key);
+                if (comp < 0)
+                {
+                    node = node.Left;
+                }
+                else if (comp > 0)
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    return node.Value;
+                }
+            }
+            return default;
+        }
+
+        public void Put(Key key, Value value)
+        {
+            Root = Put(Root, key, value);
+            Root.Color = BLACK;
+        }
+
+        private Node Put(Node node, Key key, Value value)
+        {
+            if (node == null)
+            {
+                return new Node(key, value, RED);
+            }
+
+            int comp = key.CompareTo(node.Key);
+
+            if (comp < 0)
+            {
+                node.Left = Put(node.Left, key, value);
+            }
+            else if (comp > 0)
+            {
+                node.Right = Put(node.Right, key, value);
+            }
+            else
+            {
+                node.Value = value;
+            }
+
+            if (IsRed(node.Right) && !IsRed(node.Left))
+            {
+                node = RotateLeft(node);
+            }
+            if (IsRed(node.Left) && IsRed(node.Left.Left))
+            {
+                node = RotateRight(node);
+            }
+            if (IsRed(node.Left) && IsRed(node.Right))
+            {
+                FlipColors(node);
+            }
+
+            return node;
+        }
+
+        private Node RotateLeft(Node node)
+        {
+            if (!IsRed(node.Right))
+            {
+                throw new InvalidOperationException("Right child is not red.");
+            }
+            Node x = node.Right;
+            node.Right = x.Left;
+            x.Left = node;
+            x.Color = node.Color;
+            node.Color = RED;
+            return x;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            if (!IsRed(node.Left))
+            {
+                throw new InvalidOperationException("Left child is not red.");
+            }
+            Node x = node.Left;
+            node.Left = x.Right;
+            x.Right = node;
+            x.Color = node.Color;
+            node.Color = RED;
+            return x;
+        }
+
+        private void FlipColors(Node node)
+        {
+            if (IsRed(node))
+            {
+                throw new InvalidOperationException("Node is red.");
+            }
+            if (!IsRed(node.Left) || !IsRed(node.Right))
+            {
+                throw new InvalidOperationException("Both children are not red.");
+            }
+
+            node.Left.Color = BLACK;
+            node.Right.Color = BLACK;
+            node.Color = RED;
+        }
+
+        private bool IsRed(Node node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            return node.Color == RED;
+        }
+
+        public IEnumerable<Key> Keys()
+        {
+            Queue<Key> queue = new Queue<Key>();
+            LevelOrderTraversal(Root, queue);
+            return queue;
+        }
+
+        private void LevelOrderTraversal(Node node, Queue<Key> queue)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            LevelOrderTraversal(node.Left, queue);
+            queue.Enqueue(node.Key);
+            LevelOrderTraversal(node.Right, queue);
+        }
+    }
+}
